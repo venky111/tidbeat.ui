9696630efde6ac5e0fcbc6dec334b1e5fe7d9728
venky111 <yalla.venky111@gmail.com>
2020-04-25 18:23:52 +0530

    Add files via upload

---
+++ tidbeatweb/build/precache-manifest.665cba607d3d7dfda08f0c68ca729995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1384b50b85afbb0bba00d359cdad4b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161bcfacda626a9d20d4",
+    "url": "/static/js/2.05cc20c9.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.05cc20c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8cce4adf3c458f121b6",
+    "url": "/static/js/main.6017d005.chunk.js"
+  },
+  {
+    "revision": "5333f847e389811ab256",
+    "url": "/static/js/runtime-main.32fa88ae.js"
+  }
+]);
---
+++ tidbeatweb/src/App.tsx
@@ -0,0 +1,3 @@
+import { App } from '@tidbeat/common';
+
+export default App;
